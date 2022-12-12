50d0f5e1a74f1eba98a836677f360c005407c6d8
Edgar Arakelian <edgar_arakelian@epam.com>
2022-12-12 13:33:27 +0100

    fixed

---
+++ FirstMavenProject/Hw1/src/test/java/com/epam/tc/hw1/Abstract.java
@@ -0,0 +1,22 @@
+package com.epam.tc.hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Abstract {
+
+    protected Calculator calculator;
+
+    @BeforeMethod
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @AfterMethod
+    public void after(){
+        calculator = null;
+    }
+
+}
+
