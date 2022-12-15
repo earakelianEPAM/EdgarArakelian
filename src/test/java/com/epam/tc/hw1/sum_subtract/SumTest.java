ce477574bd91727095ef9902766c90a4a6785655
Edgar Arakelian <edgar_arakelian@epam.com>
2022-12-15 17:30:00 +0100

    Structure fixing

---
+++ src/test/java/com/epam/tc/hw1/sum_subtract/SumTest.java
@@ -0,0 +1,30 @@
+package com.epam.tc.hw1.sum_subtract;
+
+import com.epam.tc.hw1.Abstract;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SumTest extends Abstract {
+
+    @DataProvider
+    public static Object[][] sumCorrectData() {
+        return new Object[][]{
+                {100.0, 100.0, 200.0},
+                {7.3, 3.3, 10.6},
+                {0.0, 2.5, 2.5},
+                {5.0, 5.0, 10.0},
+                {-100.0, -50.0, -150.0},
+                {-100.0, 50.0, -50.0},
+                {0.0, 0.0, 0.0}
+        };
+    }
+
+    @Test(dataProvider = "sumCorrectData")
+    public void subDDtTest(double a, double b, double res) {
+        var act = calculator.sum(a, b);
+        assertThat(res).as("Wrong calculation").isEqualTo(act);
+
+    }
+}
