391a7e0dd28cc1b9f30160e4f2e7a803268f10cd
Edgar Arakelian <edgar_arakelian@epam.com>
2022-12-15 17:16:02 +0100

    gitignore fixing

--- FirstMavenProject/src/test/java/com/epam/tc/hw1/Abstract.java
+++ FirstMavenProject/src/test/java/com/epam/tc/hw1/Abstract.java
@@ -9,13 +9,11 @@ public class Abstract {
     protected Calculator calculator;
 
     @BeforeMethod
-    public void before(){
-        calculator = new Calculator();
+    public void before() {calculator = new Calculator();
     }
 
     @AfterMethod
-    public void after(){
-        calculator = null;
+    public void after() {calculator = null;
     }
 
 }
